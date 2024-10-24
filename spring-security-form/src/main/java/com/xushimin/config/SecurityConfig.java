f29a7eed747ccec814ae3c09ab84f7dec78b81f7
x30044283 <xsmchengdu@163.com>
2024-10-24 19:46:39 +0800

    自定义认证失败处理器，通过返回JSON字符串适应前后端分离

--- spring-security-form/src/main/java/com/xushimin/config/SecurityConfig.java
+++ spring-security-form/src/main/java/com/xushimin/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.xushimin.config;
 
+import com.xushimin.handler.MyAuthenticationFailureHandler;
 import com.xushimin.handler.MyAuthenticationSuccessHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login.html") // 当需要登录的时候让框架返回重定向到登录页面的302响应
                 .loginProcessingUrl("/doLogin") // 不需要应用提供表单登录的接口，由框架提供登录功能，直接配置登录的接口即可让框架匹配即可。问题：框架还提供了哪些接口功能，并且运行配置，是不是还有一个logout？
                 .successHandler(new MyAuthenticationSuccessHandler()) // 自定义登录成功处理器
-                .failureForwardUrl("/mylogin.html") // 通过后端转发可以展示错误信息，failureUrl的原理是302重定向
+                .failureHandler(new MyAuthenticationFailureHandler()) // 自定义登录失败处理器
                 .usernameParameter("uname") // 表单登录的参数是formData，从该参数字段获取userName
                 .passwordParameter("passwd") // 表单登录的参数是formData，从该参数字段获取password
                 .permitAll()
---
+++ spring-security-form/src/main/java/com/xushimin/handler/MyAuthenticationFailureHandler.java
@@ -0,0 +1,28 @@
+package com.xushimin.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception)
+            throws IOException, ServletException {
+        response.setContentType("application/json;charset=utf-8");
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("status", 500);
+        resp.put("msg", "登录失败!" + exception.getMessage());
+        ObjectMapper om = new ObjectMapper();
+        String s = om.writeValueAsString(resp);
+        response.getWriter().write(s);
+    }
+}
